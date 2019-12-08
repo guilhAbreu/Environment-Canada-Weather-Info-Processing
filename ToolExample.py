689dc7e43bcdff5e90dd0e81a957c7229defcbd5
Guilherme Abreu <guiii88@outlook.com>
2019-12-08 16:01:42 -0300

    Now I can hide code and show just the output on jupyter. Tool example just download data

--- ToolExample.py
+++ ToolExample.py
@@ -16,31 +16,5 @@ metaData = pd.read_csv("stations_inventory.csv")
 quebecStations = metaData[metaData["Province"] == "QUEBEC"]
 IDs = quebecStations["Station ID"].unique()
 
-#It downloads data
-ecl.downloadData(IDs = [5205], start = (2018, 2), end = (2018,3), method = 'daily')
-
-dataSet = dict()
-dataPath = "../data_set/EnviromentCanada/QuebecStations/"
-
-#It gets the name of each file downloaded previously
-pipe = os.popen("ls -N " + dataPath)
-stationFiles = pipe.readlines()
-stationFiles = [file.replace('\n','') for file in stationFiles]
-
-#It opens them
-for file in stationFiles:
-    data = pd.read_csv(dataPath+file)
-    data = data.dropna(axis = 0, how = 'all')
-    dataSet[file.replace('.csv','')] = data
-
-#It computes how many relevant data are missing on the dataset
-countTemp = countRH = countDP   = 0
-for key in dataSet:
-    data = dataSet[key]
-    countTemp += int(data['Temp (°C)'].isna().all())
-    countRH   += int(data['Rel Hum (%)'].isna().all())
-    countDP   += int(data['Dew Point Temp (°C)'].isna().all())
-    
-print('Missing Data:\nTemp: %d of %d\nRH:   %d of %d\nDP:   %d of %d' %(countTemp,len(dataSet),
-                                                                        countRH,len(dataSet),
-                                                                        countDP,len(dataSet)))
+#It downloads data from 06/2018 to 08/2018 of the Quebec Stations
+ecl.downloadData(IDs = IDs, start = (2018, 6), end = (2018,8), method = 'daily', path='/home/guilherme/Documents/GIT REPOSITORIES/data_set/EnviromentCanada/QuebecStations/daily/')
