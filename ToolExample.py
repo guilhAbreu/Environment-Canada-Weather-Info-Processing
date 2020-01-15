9cf98de73ec14329fc9ab48d8c72d99eb7d186b2
Guilherme Abreu <guiii88@outlook.com>
2020-01-14 21:21:21 -0300

    update example

--- ToolExample.py
+++ ToolExample.py
@@ -11,46 +11,44 @@ import pandas as pd
 import envcanlib as ecl
 import numpy as np
 
+def countData(dataFrame, dictionary, column, dataQuantity):
+    '''
+    Description: It calculate the pecentage of data available  
+    '''
+    dataSet = dictionary.copy()
+    newdf = dataFrame.copy()
+    pct = list()
+    for station in dataSet:
+        data = dataSet[station]
+        pct.append(round(100*data[column].count()/dataQuantity,2))
+    
+    newdf["Data Available "+column] = pct
+    return newdf
+
 method       = 'daily'
 dataPath     = "../data_set/EnviromentCanada/QuebecStations/"+method+"/"
 
 #It opens the metadata and selects the Ids from the Province of Quebec.
 metaData = pd.read_csv("stations_inventory.csv")
-quebecStations = metaData[metaData["Province"] == "QUEBEC"]
-IDs = quebecStations["Station ID"].unique()
+quebecStations = metaData[(metaData["Province"] == "QUEBEC") & (metaData["DLY Last Year"] >= 2019)]
+
+IDs = quebecStations['Station ID'].unique()
 
 #It downloads data from 06/2018 to 08/2018 of the Quebec Stations
-#ecl.downloadData(IDs = IDs, start = (2018, 6), end = (2018,8), method = 'daily', path='/home/guilherme/Documents/GIT REPOSITORIES/data_set/EnviromentCanada/QuebecStations/daily/')
+ecl.downloadData(IDs = IDs, start = (2019, 6), end = (2019,8), method = method, 
+                 path='/home/guilherme/Documents/GIT REPOSITORIES/data_set/EnviromentCanada/QuebecStations/'+method+'/2019 - Jun2Aug/')
 
 dataSet = dict()
 
 #It gets the name of each file downloaded previously
-stationFiles = os.listdir(dataPath)
+stationFiles = quebecStations['Station ID'].unique().astype('str')
 
 #It opens them
 for file in stationFiles:
-    data = pd.read_csv(dataPath+file)
+    data = pd.read_csv(dataPath+'Heatwave/'+file+'.csv')
     data = data.dropna(axis = 0, how = 'all')
-    dataSet[file.replace('.csv','')] = data
+    dataSet[file] = data
 
-#It computes how many relevant data are missing on the dataset
-countmaxTemp = countminTemp = 0
-missingData = pd.DataFrame([], columns = ['Station ID', 'Station Name', 'Has Max Temp', 'Has Min Temp', 'lat', 'lng'])
-missingData['Station ID']   = (quebecStations['Station ID'].values).astype('str')
-missingData['Station Name'] = quebecStations['Name'].values
-missingData['lat']          = np.asarray(quebecStations['Latitude (Decimal Degrees)'].values)
-missingData['lng']          = np.asarray(quebecStations['Longitude (Decimal Degrees)'].values)
-for key in dataSet:
-    data = dataSet[key]
-    
-    nomaxTmp = data['Max Temp (°C)'].isna().all()
-    nominTmp = data['Min Temp (°C)'].isna().all()
-    countmaxTemp += int(nomaxTmp)
-    countminTemp += int(nominTmp)
-    missingData.loc[missingData['Station ID'] == key,'Has Max Temp'] = not nomaxTmp
-    missingData.loc[missingData['Station ID'] == key,'Has Min Temp'] = not nominTmp
-
-dataLength = len(dataSet)
-print('Missing Data in %d stations:' %dataLength)
-print('Max Air Temperature:     %.2f'  %(100*countmaxTemp/dataLength) + '%')
-print('Min Air Temperature:     %.2f'  %(100*countminTemp/dataLength) + '%')
+from datetime import date
+delta = date(2018, 8, 31) - date(2018, 6, 1)
+quebecStations = countData(quebecStations, dataSet, 'Max Temp (°C)', (delta.days + 1))
