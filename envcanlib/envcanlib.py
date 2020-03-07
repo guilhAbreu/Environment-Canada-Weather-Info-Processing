c8a6c841007d01976f6738f7740fa6d42d03d551
Guilherme Abreu <guiii88@outlook.com>
2020-03-07 01:19:18 -0300

    update envcanlib. now it is possible download data and store in one file all stations information or one file per station. Also pull.py was updated to work with this new feature

--- envcanlib/envcanlib.py
+++ envcanlib/envcanlib.py
@@ -6,7 +6,7 @@ Created on Wed Nov 30 23:35:15 2019
 @author: guilherme
 """
 
-def downloadData(IDs, start, end, method = 'hourly', path = './'):
+def downloadData(IDs, start, end, method = 'hourly', path = '', dataFormat = 'default', continuous = True):
     '''
     Description: It downloads weather information from the Environment Canada website. 
     It is possible to download daily or hourly information in a slice of time passed as an argument.
@@ -19,7 +19,9 @@ def downloadData(IDs, start, end, method = 'hourly', path = './'):
            
            method: 'hourly' for hourly information (default) or 'daily' for daily information.
            
-           path: Path in the machine to save the data downloaded.
+           path: Path in the machine to save the data downloaded. Default is the path where the code is running.
+           
+           dataFormat: 'default' (each station has its own file) or 'oneFile' (just one file for all stations).
     '''
     
     import pandas as pd
@@ -38,25 +40,56 @@ def downloadData(IDs, start, end, method = 'hourly', path = './'):
         print('method = ' + method + 'is not valid.')
         print('avalible methods are "h" or "d".')
     
-    for ID in IDs:
-        data = pd.DataFrame([])
-        for intYr in range(start[0], end[0]+1):
-            if intYr == start[0]:
-                if start[0] == end[0]:
-                    monthRange = range(start[1], end[1]+1)
+    if dataFormat == 'default':
+        for ID in IDs:
+            data = pd.DataFrame([])
+            for intYr in range(start[0], end[0]+1):
+                if continuous:
+                    if intYr == start[0]:
+                        if start[0] == end[0]:
+                            monthRange = range(start[1], end[1]+1)
+                        else:
+                            monthRange = range(start[1],13)
+                    elif intYr == end[0]:
+                        monthRange = range(1, end[1]+1)
+                    else:
+                        monthRange = range(1, 13)
                 else:
-                    monthRange = range(start[1],13)
-            elif intYr == end[0]:
-                monthRange = range(1, end[1]+1)
-            else:
-                monthRange = range(1, 13)    
-            
-            if method == "&timeframe=1&submit=Download+Data":
-                for intMnt in monthRange:
+                    monthRange = range(start[1],end[1]+1)
+                
+                if method == "&timeframe=1&submit=Download+Data":
+                    for intMnt in monthRange:
+                        #build the query
+                        strQry = 'http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=' + str(ID) + "&Year=" + str(intYr) +'&Month=' + str(intMnt) + method 
+                        #print strQry
+                        print ('Querying station ' + str(ID) + ' for year ' + str(intYr) + ' and month ' + str(intMnt))
+                        try:
+                            response = url.urlopen(strQry)
+                            rawData = response.readlines()
+                            response.close()
+                            rawData = [row.decode('utf8').replace('"','').replace('\n','') for row in rawData]
+                           
+                            columns = rawData[0].split(',')
+                            d = [line.split(',') for line in rawData[1:]]
+                            
+                            for i in range(len(d)):
+                                if len(d[i]) > len(columns):
+                                    d[i][len(columns)-1] = "".join(d[i][len(columns)-1:])
+                                    d[i] = d[i][:len(columns)]
+                                    
+                                if len(d[i]) < len(columns):
+                                    while len(d[i]) < len(columns):
+                                        d[i].append('')
+                                    
+                            newData = pd.DataFrame(d, columns=columns)
+                            data = data.append(newData, ignore_index=True, sort=False)
+                        except Exception as e:
+                            print ('Failure getting data for '  + str(ID) + ' for year ' + str(intYr) + '. ',e)
+                else:
                     #build the query
-                    strQry = 'http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=' + str(ID) + "&Year=" + str(intYr) +'&Month=' + str(intMnt) + method 
+                    strQry = 'http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=' + str(ID) + "&Year=" + str(intYr) + method 
                     #print strQry
-                    print ('Querying station ' + str(ID) + ' for year ' + str(intYr) + ' and month ' + str(intMnt))
+                    print ('Querying station ' + str(ID) + ' for year ' + str(intYr))
                     try:
                         response = url.urlopen(strQry)
                         rawData = response.readlines()
@@ -77,44 +110,106 @@ def downloadData(IDs, start, end, method = 'hourly', path = './'):
                                 
                         newData = pd.DataFrame(d, columns=columns)
                         data = data.append(newData, ignore_index=True, sort=False)
-                    except Exception as e:
-                        print ('Failure getting data for '  + str(ID) + ' for year ' + str(intYr) + '. ',e)
-            else:
-                #build the query
-                strQry = 'http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=' + str(ID) + "&Year=" + str(intYr) + method 
-                #print strQry
-                print ('Querying station ' + str(ID) + ' for year ' + str(intYr))
-                try:
-                    response = url.urlopen(strQry)
-                    rawData = response.readlines()
-                    response.close()
-                    rawData = [row.decode('utf8').replace('"','').replace('\n','') for row in rawData]
-                   
-                    columns = rawData[0].split(',')
-                    d = [line.split(',') for line in rawData[1:]]
-                    
-                    for i in range(len(d)):
-                        if len(d[i]) > len(columns):
-                            d[i][len(columns)-1] = "".join(d[i][len(columns)-1:])
-                            d[i] = d[i][:len(columns)]
-                            
-                        if len(d[i]) < len(columns):
-                            while len(d[i]) < len(columns):
-                                d[i].append('')
-                            
-                    newData = pd.DataFrame(d, columns=columns)
-                    data = data.append(newData, ignore_index=True, sort=False)
-                    data['Month'] = data['Month'].astype(int)
-                    data['Year'] = data['Year'].astype(int)
-                    if start[0] != end[0]:
-                        data = data[((data['Month'] >= start[1]) & (data['Year'] == start[0]))
-                                    | ((data['Year'] != start[0]) & (data['Year'] != end[0]))
-                                    | ((data['Month'] <= end[1]) & (data['Year'] == end[0]))]
+                        data['Month'] = data['Month'].astype(int)
+                        data['Year'] = data['Year'].astype(int)
+                        if start[0] != end[0]:
+                            data = data[((data['Month'] >= start[1]) & (data['Year'] == start[0]))
+                                        | ((data['Year'] != start[0]) & (data['Year'] != end[0]))
+                                        | ((data['Month'] <= end[1]) & (data['Year'] == end[0]))]
+                        else:
+                            data = data[(data['Month'] >= start[1]) & (data['Month'] <= end[1])]
+    
+                    except Exception:
+                        print ('Failure getting data for '  + str(ID) + ' for year ' + str(intYr))
+            
+            data = data.dropna(axis = 0, how = 'all')
+            data.to_csv(path+str(ID)+".csv", index=False, line_terminator="")
+            
+    else:
+        data = pd.DataFrame([])
+        for ID in IDs:
+            for intYr in range(start[0], end[0]+1):
+                if continuous:
+                    if intYr == start[0]:
+                        if start[0] == end[0]:
+                            monthRange = range(start[1], end[1]+1)
+                        else:
+                            monthRange = range(start[1],13)
+                    elif intYr == end[0]:
+                        monthRange = range(1, end[1]+1)
                     else:
-                        data = data[(data['Month'] >= start[1]) & (data['Month'] <= end[1])]
-
-                except Exception:
-                    print ('Failure getting data for '  + str(ID) + ' for year ' + str(intYr))
-        
+                        monthRange = range(1, 13)
+                else:
+                    monthRange = range(start[1],end[1]+1)    
+                
+                if method == "&timeframe=1&submit=Download+Data":
+                    for intMnt in monthRange:
+                        #build the query
+                        strQry = 'http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=' + str(ID) + "&Year=" + str(intYr) +'&Month=' + str(intMnt) + method 
+                        #print strQry
+                        print ('Querying station ' + str(ID) + ' for year ' + str(intYr) + ' and month ' + str(intMnt))
+                        try:
+                            response = url.urlopen(strQry)
+                            rawData = response.readlines()
+                            response.close()
+                            rawData = [row.decode('utf8').replace('"','').replace('\n','') for row in rawData]
+                           
+                            columns = rawData[0].split(',')
+                            d = [line.split(',') for line in rawData[1:]]
+                            
+                            for i in range(len(d)):
+                                if len(d[i]) > len(columns):
+                                    d[i][len(columns)-1] = "".join(d[i][len(columns)-1:])
+                                    d[i] = d[i][:len(columns)]
+                                    
+                                if len(d[i]) < len(columns):
+                                    while len(d[i]) < len(columns):
+                                        d[i].append('')
+                                    
+                            newData = pd.DataFrame(d, columns=columns)
+                            newData.insert(0, column='Station ID', value = [ID for i in range(newData.shape[0])])
+                            
+                            data = data.append(newData, ignore_index=True, sort=False)
+                        except Exception as e:
+                            print ('Failure getting data for '  + str(ID) + ' for year ' + str(intYr) + '. ',e)
+                else:
+                    #build the query
+                    strQry = 'http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=' + str(ID) + "&Year=" + str(intYr) + method 
+                    #print strQry
+                    print ('Querying station ' + str(ID) + ' for year ' + str(intYr))
+                    try:
+                        response = url.urlopen(strQry)
+                        rawData = response.readlines()
+                        response.close()
+                        rawData = [row.decode('utf8').replace('"','').replace('\n','') for row in rawData]
+                       
+                        columns = rawData[0].split(',')
+                        d = [line.split(',') for line in rawData[1:]]
+                        
+                        for i in range(len(d)):
+                            if len(d[i]) > len(columns):
+                                d[i][len(columns)-1] = "".join(d[i][len(columns)-1:])
+                                d[i] = d[i][:len(columns)]
+                                
+                            if len(d[i]) < len(columns):
+                                while len(d[i]) < len(columns):
+                                    d[i].append('')
+                                
+                        newData = pd.DataFrame(d, columns=columns)
+                        newData.insert(0, column='Station ID', value = [ID for i in range(newData.shape[0])])
+                        
+                        data = data.append(newData, ignore_index=True, sort=False)
+                        data['Month'] = data['Month'].astype(int)
+                        data['Year'] = data['Year'].astype(int)
+                        if start[0] != end[0]:
+                            data = data[((data['Month'] >= start[1]) & (data['Year'] == start[0]))
+                                        | ((data['Year'] != start[0]) & (data['Year'] != end[0]))
+                                        | ((data['Month'] <= end[1]) & (data['Year'] == end[0]))]
+                        else:
+                            data = data[(data['Month'] >= start[1]) & (data['Month'] <= end[1])]
+    
+                    except Exception:
+                        print ('Failure getting data for '  + str(ID) + ' for year ' + str(intYr))
+            
         data = data.dropna(axis = 0, how = 'all')
-        data.to_csv(path+str(ID)+".csv", index=False, line_terminator="")
+        data.to_csv(path+'allStations.csv', index=False, line_terminator="")        
--- pull.py
+++ pull.py
@@ -3,36 +3,56 @@
 """
 Created on Sun Nov 24 02:02:42 2019
 
-@author: guilherme
+@author: Guilherme de Brito Abreu
+@email: debritoabreu@gmail.com
+
+Description: It downloads weather information from the Environment Canada website. 
+It is possible to download daily or hourly information in a slice of time passed as an argument.
+The Time Series passed can be continuous ou not continuous.
+
+The arguments should be like the following:
+    
+         <STARTYEAR> <STARTMONTH> <ENDYEAR> <ENDMONTH> <PATH>
+         <StationsListFile> <METHOD> <FORMAT> <ContinuousTimeSeriesFlag>
+         
+where <STARTYEAR> <STARTMONTH> <ENDYEAR> <ENDMONTH> is the period of the interest passed as numbers,
+<PATH> is the path on the machine which the files will be stored. <StationsListFile> is the name of
+file containing each Station ID per row to be downloaded. <METHOD> can be 'hourly' or 'daily' which 
+means what type of information will be downloaded. <FORMAT> speciefy the format of the data. The 
+options are 'default', which means one file per station,  and 'oneFile', which means one file for 
+all stations. <ContinuousTimeSeriesFlag> indicates whether the period of time passed is continuous 
+or not.
 """
 
 import os, sys
 import envcanlib as ecl
 
-if len(sys.argv) < 7:
-    quit('Missing Arguments.The arguments should be like the following\n'+
-         '<STARTYEAR> <ENDYEAR> <STARTMONTH> <ENDMONTH> <PATH> <STATIONSLISTFILE> <METHOD(hourly or daily)>')
+if __name__ == '__main__':
+    if len(sys.argv) < 9:
+        quit('Missing Arguments.The arguments should be like the following\n'+
+             '<STARTYEAR> <ENDYEAR> <STARTMONTH> <ENDMONTH> <METHOD>'+
+             '<PATH> <StationsListFile> <FORMAT> <ContinuousTimeSeriesFlag>')
+        
+    startYear  = int(sys.argv[1])
+    startMonth = int(sys.argv[2])
+    endYear    = int(sys.argv[3])
+    endMonth   = int(sys.argv[4])
+    method     = sys.argv[5]
+    dataPath   = sys.argv[6]+method+"/"
+    file       = sys.argv[7]
+    dataFormat = sys.argv[8]
+    conFlag    = True if sys.argv[9] == 'True' else False
     
-startYear  = int(sys.argv[1])
-endYear    = int(sys.argv[2])
-startMonth = int(sys.argv[3])
-endMonth   = int(sys.argv[4])
-method     = sys.argv[7]
-dataPath   = sys.argv[5]+method+"/"
-file       = sys.argv[6]
-
-
-stationsListF = open(file, 'r')
-IDs = stationsListF.readlines()
-stationsListF.close()
-
-IDs = [ID.replace('\n','') for ID in IDs]
-
-for year in range(startYear, endYear+1):    
+    stationsListF = open(file, 'r')
+    IDs = stationsListF.readlines()
+    stationsListF.close()
+    
+    IDs = [ID.replace('\n','') for ID in IDs]
+    
     try:
-        os.makedirs(dataPath+str(year))
+        os.makedirs(dataPath)
     except FileExistsError:
         pass
     
-    ecl.downloadData(IDs = IDs, start = (year, startMonth), end = (year, endMonth), 
-                     method = method, path=dataPath+str(year)+'/')
+    ecl.downloadData(IDs = IDs, start = (startYear, startMonth), end = (endYear, endMonth), method = method, 
+                     path=dataPath, dataFormat = dataFormat, continuous = conFlag)
