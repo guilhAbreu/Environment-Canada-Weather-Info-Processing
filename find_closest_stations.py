24adef2a98af59ef85c1dc1798ec6288d38fa645
unknown <guiii88@outlook.com>
2020-08-11 15:50:10 -0300

    complete database with the closest stations that have hourly data and update repository with new data results

--- find_closest_stations.py
+++ find_closest_stations.py
@@ -30,19 +30,12 @@ import numpy as np
 import envcanlib
 
 #get IDs of the stations of interest
-f = open('WEATHER_STATIONS.txt', 'r')
-stations = f.readlines()
-f.close()
-stations = [s.replace('\n','') for s in stations]
+IDs = ['51157', '53001']
 
 #open and filter what stations have information on the period of interest
 md = pd.read_csv("stations_inventory.csv")
 quebecStations = md[md['Province'] == 'QUEBEC']
-six2ntyStations = quebecStations[(quebecStations['DLY First Year'] <= 1981) & (quebecStations['DLY Last Year'] >= 2010)]
-
-f = open('WEATHER_STATIONS_IDs.txt', 'r')
-IDs = f.readlines()
-f.close()
+possStations = quebecStations[(quebecStations['HLY First Year'] <= 2010) & (quebecStations['HLY Last Year'] >= 2012)]
 
 #get the information of the stations of interest
 IDs = [int(ID) for ID in IDs]
@@ -50,28 +43,29 @@ hw_stations = md[md['Station ID'].isin(IDs)]
 
 #create new one columns for each station of interest
 for ID in IDs:
-    six2ntyStations.insert(0, column=str(ID), 
-                           value = [np.nan for i in range(six2ntyStations.shape[0])])
+    possStations.insert(0, column=str(ID), 
+                           value = [np.nan for i in range(possStations.shape[0])])
 
 #compute the distance in coordinates for each station
-for index1,row1 in six2ntyStations.iterrows():
+for index1,row1 in possStations.iterrows():
     x1,y1 = row1['Latitude (Decimal Degrees)'], row1['Longitude (Decimal Degrees)']
     for index2, row2 in hw_stations.iterrows():
         x2,y2 = row2['Latitude (Decimal Degrees)'], row2['Longitude (Decimal Degrees)']
         
         dist = distanceCalculator(x1, y1, x2, y2)
         
-        six2ntyStations.loc[(six2ntyStations['Station ID'] == row1['Station ID']), 
+        possStations.loc[(possStations['Station ID'] == row1['Station ID']), 
                             str(row2['Station ID'])] = dist
 
 #create a dictionary to store the closest station to each station of interest
 data = list()
 for i in range(len(IDs)):
-    temp = six2ntyStations.sort_values(by=[str(IDs[i])], ignore_index=True)
+    temp = possStations.sort_values(by=[str(IDs[i])], ignore_index=True)
     data.append([str(IDs[i]), str(temp['Station ID'][0]), round(temp[str(IDs[i])][0], 2)])
     
 new_table = pd.DataFrame(data = data, columns=['Station ID', 'Closest Station ID', 'Distance (Km)'])
-new_table.to_csv('closest_stations_81-2010.csv')
+new_table.to_csv('closest_stations_2010-2012.csv')
 
-envcanlib.downloadData(IDs = new_table['Closest Station ID'].unique(), start=(1981,1), end=(2010,12), method='daily',
-                       path='/home/guilherme/Documents/GIT - REPOSITORIES/dataset/EnvironmentCanada/Quebec/climateNormals/1981-2010/')
+envcanlib.downloadData(IDs = ['30165'], start=(2010,6), end=(2012,8), method='daily', continuous = False)
+
+envcanlib.downloadData(IDs = ['5574'], start=(2010,6), end=(2014,8), method='daily', continuous = False)
